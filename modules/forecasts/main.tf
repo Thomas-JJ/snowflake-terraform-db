fdcfc742e15a3e94a21bf3bb21a0033fa724a19d
Tom Johnson <Thomas.j41.johnson@gmail.com>
2025-10-01 06:37:31 -0400

    Update(Forecasts): Standard table to store results created in forecasts module

--- modules/forecasts/main.tf
+++ modules/forecasts/main.tf
@@ -40,6 +40,60 @@ resource "snowflake_execute" "forecast" {
   depends_on = [ snowflake_view.this ]
 }
 
+resource "snowflake_table" "forecast_results" {
+  for_each = var.forecasts
+  
+  database = var.database_name
+  schema   = each.value.schema
+  name     = each.value.forecast_results_table
+  change_tracking = true
+
+  column {
+    name = "SERIES"
+    type = "VARCHAR(255)"
+    nullable = false
+  }
+
+  column {
+    name = "TS"
+    type = "TIMESTAMP_NTZ"
+    nullable = false
+  }
+
+  column {
+    name = "FORECAST"
+    type = "FLOAT"
+    nullable = false
+  }
+
+  column {
+    name = "LOWER_BOUND"
+    type = "FLOAT"
+    nullable = false
+  }
+
+  column {
+    name = "UPPER_BOUND"
+    type = "FLOAT"
+    nullable = false
+  }
+
+  column {
+    name = "LOAD_ID"
+    type = "VARCHAR(36)"
+    nullable = false
+  }
+
+  column {
+    name = "LOAD_DATE"
+    type = "TIMESTAMP_NTZ"
+    nullable = false
+  }
+
+  comment = "Forecast resutls for ${each.key}"
+
+  depends_on = [ snowflake_execute.forecast ]
+}
 
 # Task to run cron job automatically
 resource "snowflake_task" "copy_task" {
@@ -56,7 +110,7 @@ resource "snowflake_task" "copy_task" {
   }
 
   sql_statement = format(
-    "INSERT INTO %s.%s.%s (SERIES, TS, FORECAST, LOWER_BOUND, UPPER_BOUND, LOAD_ID, LOAD_DATE) WITH LOAD_ID_COL AS (SELECT UUID_STRING() AS LOAD_ID) SELECT f.SERIES, f.TS, f.FORECAST, f.LOWER_BOUND, f.UPPER_BOUND, l.LOAD_ID, CURRENT_DATE() as LOAD_DATE FROM TABLE(%s.%s.%s!FORECAST(FORECASTING_PERIODS => %s)) f CROSS JOIN LOAD_ID_COL l",
+    "INSERT INTO %s.%s.%s (SERIES, TS, FORECAST, LOWER_BOUND, UPPER_BOUND, LOAD_ID, LOAD_DATE) WITH LOAD_ID_COL AS (SELECT UUID_STRING() AS LOAD_ID) SELECT f.SERIES, f.TS, f.FORECAST, f.LOWER_BOUND, f.UPPER_BOUND, l.LOAD_ID, CURRENT_TIMESTAMP() as LOAD_DATE FROM TABLE(%s.%s.%s!FORECAST(FORECASTING_PERIODS => %s)) f CROSS JOIN LOAD_ID_COL l",
 
     var.database_name,
     each.value.schema,
@@ -70,6 +124,6 @@ resource "snowflake_task" "copy_task" {
 
   started = true
   
-  depends_on = [ snowflake_execute.forecast ]
+  depends_on = [ snowflake_table.forecast_results ]
 }
 
